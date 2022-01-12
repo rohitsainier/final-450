1b48dae93c6c108ecec14093ddddeb739b81dd91
rohitsainier <rohitsainier@gmail.com>
2022-01-12 14:20:25 +0530

    Comment added in union algo

--- Union_Of_Two_Array.swift
+++ Union_Of_Two_Array.swift
@@ -5,6 +5,7 @@ var arr2 = [85, 2]
 
 func union(){
     for i in 0..<arr1.count{
+        //Checking the common elements in both array
         if arr2.contains(arr1[i]){
             print(arr1[i])
         }
@@ -13,6 +14,7 @@ func union(){
         }
     }
     for j in 0..<arr2.count{
+        //print only those element which are not present in array2
         if !arr1.contains(arr2[j]){
             print(arr2[j])
         }
