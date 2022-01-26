18d275e5b472963b023fe2413dd5737bbff15333
rohitsainier <rohitsainier@gmail.com>
2022-01-26 17:28:45 +0530

    find Largest sum contiguous Subarray

---
+++ find_largest_sum_continuos_subarray.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+var arr = [1,2,3,-2,5,]
+var max_ending_here = 0
+var max_so_far = 0
+for num in arr{
+    max_ending_here = max_ending_here + num
+    if max_so_far < max_ending_here{
+        max_so_far = max_ending_here
+    }
+    if max_ending_here < 0{
+        max_ending_here = 0
+    }
+}
+print(max_so_far)
